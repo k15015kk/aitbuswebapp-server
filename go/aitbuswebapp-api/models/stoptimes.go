03114143c4b8fbd6a4270d69aa332e0f7ea0b91d
k15015kk <aitk15015kk@outlook.com>
2021-09-15 23:41:42 +0900

    ModelのIDを定義

--- go/aitbuswebapp-api/models/stoptimes.go
+++ go/aitbuswebapp-api/models/stoptimes.go
@@ -2,25 +2,21 @@ package models
 
 import (
 	"aitbuswebapp-api/database"
-	"fmt"
 )
 
-// TODO: ここに問題がありそう
 type StopTimes struct {
-	trip_id        string
-	arrival_time   string
-	departure_time string
-	stop_id        string
-	stop_sequence  int
-	pickup_type    int
-	drop_off_type  int
+	TripId        string
+	ArrivalTime   string
+	DepartureTime string
+	StopId        string
+	StopSequence  int
+	PickupType    int
+	DropOffType   int
 }
 
 type StopTimesArray []*StopTimes
 
 func (st *StopTimes) FindByTripId(tripId string) (err error) {
 	db := database.GetDB()
-	test := db.Where("trip_id = ?", tripId).First(st).Value
-	fmt.Printf("%#v\n", test)
 	return db.Where("trip_id = ?", tripId).First(st).Error
 }
